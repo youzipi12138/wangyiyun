dccc2780ca426e9be72f1335e12ad74b0165e977
youzipi12138 <ranren875@gmail.com>
2025-10-18 19:07:49 +0800

    feat:master

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ export default {
     extend: {
       // 手动设置你希望的 px 值
       screens: {
-        sm: '640px',
+        sm: '640px',          // master提交
         md: '768px',
         lg: '1056px',
         xl: '1253px', // 手动设置你希望的 px 值 //main分支新增代码 // 主分支又提交代码///fasdfasf
